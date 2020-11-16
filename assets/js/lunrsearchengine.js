86afd416de0451df23852f03eeffe43d38244880
ChrisHammond <ChrisHammond@users.noreply.github.com>
2020-11-16 20:31:00 +0000

    deploy: f906d2d1b585ff774d3fc350dbbc07777d9e69c2

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -838,7 +838,7 @@ var documents = [{
     "id": 167,
     "url": "/unbound-gravel-deferral-registration",
     "title": "Unbound Gravel 2021 Deferral Registration",
-    "body": "2020/11/16 - Today is November 16, 2020, why is that an important date? If you registered for the Dirty Kanza in 2020 and chose a deferral option, today is the day that deferral users have the ability to sign up for the 2021 race. Check out your email for details on registration for Unbound Gravel 2021. One side note: Personally we received an email saying that we had deferred to 2022 and wouldn’t be receiving a registration link today. Hopefully that was just an error and they’ll get it corrected today. I know personally I am planning on riding 2021. Have you started your training yet? -Chris "
+    "body": "2020/11/16 - Today is November 16, 2020, why is that an important date? If you registered for the Dirty Kanza in 2020 and chose a deferral option, today is the day that deferral users have the ability to sign up for the 2021 race. Check out your email for details on registration for Unbound Gravel 2021. One side note: Personally we received an email saying that we had deferred to 2022 and wouldn’t be receiving a registration link today. Hopefully that was just an error and they’ll get it corrected today. I know personally I am planning on riding 2021. Have you started your training yet? -Chris Update: 11/16/2020: The marketing director from LifeTime posted in a private Facebook group acknowledging that some deferrals were errantly attributed to 2022, and they will be emailing those impacted with registration information for 2021. "
     }, {
     "id": 168,
     "url": "/dirty-kanza-is-unbound",
